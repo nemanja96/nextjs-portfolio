33cdbcbc1d1131b216cd06c6be81f2aee2ccc419
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-06-26 09:05:41 +0200

    Building the Header Social Media Icons

--- components/SocialNav.js
+++ components/SocialNav.js
@@ -3,7 +3,7 @@ import {GiLetterBomb} from 'react-icons/gi';
 const SocialNav = () => {
   return (
     <div className="flex justify-center py-2">
-        <p className="text-black-500 flex items-center gap-2"><GiLetterBomb className="w-6 h-6 animate-pulse" /><span className="hidden md:inline">Pišite mi na:</span><a href="mailto:kontakt@nemanjaradivojevic.rs" target="_blank" rel="noreferrer" className="text-center underline hover:text-gray-600 hover:transition">kontakt@nemanjaradivojevic.rs</a></p>
+        <div className="text-black-500 flex items-center gap-2"><GiLetterBomb className="w-6 h-6 animate-pulse" /><span className="hidden md:inline">Pišite mi na:</span><a href="mailto:kontakt@nemanjaradivojevic.rs" target="_blank" rel="noreferrer" className="text-center underline hover:text-gray-600 hover:transition">kontakt@nemanjaradivojevic.rs</a></div>
     </div>
   )
 }
